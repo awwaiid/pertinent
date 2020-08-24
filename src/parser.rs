4666b6dbd28786de97f8472c0ee3d295d8f7546a
Brock Wilcox <awwaiid@thelackthereof.org>
2020-08-23 22:58:40 -0400

    Parse global options into SlideDeck

--- src/parser.rs
+++ src/parser.rs
@@ -24,11 +24,11 @@ extern crate nom;
 
 use nom::{
     bytes::complete::{ tag, take_until },
-    character::complete::{multispace0, alphanumeric1, newline},
+    character::complete::{multispace0, multispace1, alphanumeric1, line_ending, not_line_ending},
     sequence::{ terminated, preceded, delimited },
     multi::{ fold_many0, many0 },
     combinator::{rest, map_res},
-    branch::alt
+    branch::{alt, permutation}
 };
 
 
@@ -50,6 +50,29 @@ fn option(input: &str) -> nom::IResult<&str, &str> {
     return preceded(multispace0, delimited(tag("["), alphanumeric1, tag("]")))(input);
 }
 
+fn whitespace_or_comment(input: &str) -> nom::IResult<&str, &str> {
+    let (input, _) = many0(
+        alt((
+                multispace1,
+                preceded(
+                    tag("#"),
+                    terminated( not_line_ending, line_ending))
+                )))(input)?;
+    return Ok((input, ""));
+}
+
+fn settings(input: &str) -> nom::IResult<&str, SlideOptions> {
+    let (input, options) = terminated(
+        many0(
+            preceded( whitespace_or_comment, option )
+        ),
+        whitespace_or_comment
+    )(input)?;
+
+    let options = options.iter().map(|s| s.to_string()).collect();
+    return Ok((input, options));
+}
+
 // Slide headers look like "-- [a] [b]\n"
 fn header(input: &str) -> nom::IResult<&str, SlideOptions> {
     let option_list = many0( option );
@@ -57,7 +80,7 @@ fn header(input: &str) -> nom::IResult<&str, SlideOptions> {
     let (input, options) =
         terminated(
             preceded( tag("--"), option_list ),
-            newline
+            line_ending
             )(input)?;
 
     let options = options.iter().map(|s| s.to_string()).collect();
@@ -71,7 +94,7 @@ fn content(input: &str) -> nom::IResult<&str, &str> {
             ))(input)?;
 
     // let (input, content) = take_until("\n--")(input)?;
-    // let (input, _) = newline(input)?;
+    // let (input, _) = line_ending(input)?;
     return Ok((input, content));
 }
 
@@ -84,14 +107,40 @@ fn slide(input: &str) -> nom::IResult<&str, Slide> {
     }));
 }
 
-pub fn parse_slides(input: &str) -> nom::IResult<&str, Vec<Slide>> {
-    return Ok(many0(slide)(input)?);
+fn slides(input: &str) -> nom::IResult<&str, Vec<Slide>> {
+    return many0(slide)(input);
+}
+
+pub fn parse_slides(input: &str) -> nom::IResult<&str, SlideDeck> {
+    let (input, global_options) = settings(input)?;
+    let (input, slides) = slides(input)?;
+    return Ok((input,
+            SlideDeck {
+                global_options: global_options,
+                slides: slides
+            }
+            ));
 }
 
 #[cfg(test)]
 mod tests {
 
     use super::*;
+
+    #[test]
+    fn test_whitespace_or_comment() {
+        assert_eq!( whitespace_or_comment(""), Ok(("","")));
+        assert_eq!( whitespace_or_comment(" "), Ok(("","")));
+        assert_eq!( whitespace_or_comment("    "), Ok(("","")));
+        assert_eq!( whitespace_or_comment(" # hmm\n"), Ok(("", "")));
+        assert_eq!( whitespace_or_comment("# hmm\n"), Ok(("", "")));
+        assert_eq!( whitespace_or_comment("\n\n"), Ok(("", "")));
+    }
+
+    #[test]
+    fn test_settings() {
+        assert_eq!( settings(""), Ok(("", vec![])));
+    }
 
     #[test]
     fn header_test() {
@@ -124,9 +173,31 @@ mod tests {
     #[test]
     fn parser_test2() {
         assert_eq!(
+            parse_slides(""),
+            Ok((
+                    "",
+                    SlideDeck {
+                        global_options: vec![],
+                        slides: vec![]
+                    }
+               )));
+        assert_eq!(
             parse_slides("--\nhello world"),
             Ok((
-                    "", vec![Slide { options: vec![], content: "hello world".to_string() }]
+                    "",
+                    SlideDeck {
+                        global_options: vec![],
+                        slides: vec![Slide { options: vec![], content: "hello world".to_string() }]
+                    }
+               )));
+        assert_eq!(
+            parse_slides("[a]\n--\nhello world"),
+            Ok((
+                    "",
+                    SlideDeck {
+                        global_options: vec!["a".to_string()],
+                        slides: vec![Slide { options: vec![], content: "hello world".to_string() }]
+                    }
                )));
     }
 
